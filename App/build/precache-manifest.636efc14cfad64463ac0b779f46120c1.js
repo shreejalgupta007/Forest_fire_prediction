da917240cbad9e8956bdd33479664e77e0c1a7fe
priyansh19 <priyansh7533053411@gmail.com>
2020-05-07 11:27:23 +0530

    Adding Template for starting project

---
+++ App/build/precache-manifest.636efc14cfad64463ac0b779f46120c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f02d8dffd0108ac33233f7eea2ba168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786632848e594719f228",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31ad9298ceeff803d6ec",
+    "url": "/static/js/2.ef4d07c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef4d07c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786632848e594719f228",
+    "url": "/static/js/main.29260f4e.chunk.js"
+  },
+  {
+    "revision": "f05bbac952b8a098c675",
+    "url": "/static/js/runtime-main.39cbb1dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

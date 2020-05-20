b1ca8bb5676ccb422eb79f4d24335689d964b7d8
shreejalgupta007 <63512396+shreejalgupta007@users.noreply.github.com>
2020-05-21 01:14:44 +0530

    Update send.php

--- App/src/send.php
+++ App/src/send.php
@@ -1,7 +1,9 @@
 <?php
+//opens a new file to write
 $data_file=fopen("test1.txt","w");
 $email=$_POST["email"];
-$text_to_write=$email
+$text_to_write=$email;
+//write data to server side file
 fwrite($data_file, $text_to_write);
 fclose($data_file);
 ?>

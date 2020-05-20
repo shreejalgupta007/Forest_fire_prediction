98d29038f68da5597f6282bab2a9400a01029803
shreejalgupta007 <63512396+shreejalgupta007@users.noreply.github.com>
2020-05-21 01:13:05 +0530

    Update submit.php

--- App/src/submit.php
+++ App/src/submit.php
@@ -1,8 +1,10 @@
 <?php
+//opens a new file to write
 $data_file=fopen("test.txt","w");
 $username=$_POST["username"];
 $password=$_POST["password"];
 $text_to_write=$username." ".$password;
+//write data to server side file
 fwrite($data_file, $text_to_write);
 fclose($data_file);
 ?>

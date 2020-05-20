94e300d59b7fbecd5d16595692f067693f0c9a90
shreejalgupta007 <63512396+shreejalgupta007@users.noreply.github.com>
2020-05-21 01:21:35 +0530

    Update go.php

--- App/src/go.php
+++ App/src/go.php
@@ -5,8 +5,7 @@ $fullname=$_POST["fullname"];
 $email=$_POST["email"];
 $username=$_POST["username"];
 $password=$_POST["password"];
-$re-enter password=$_["re-enter password"];
-$text_to_write=$username." ".$password;
+$text_to_write=$fullname." ".$email." ".$username." ".$password;
 //writes on a server side file
 fwrite($data_file, $text_to_write);
 fclose($data_file);
